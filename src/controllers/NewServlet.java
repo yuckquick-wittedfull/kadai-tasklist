38d72efc4620d96d1b122e1ec47520647b44824d
ryan <email>
2023-10-28 21:04:50 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -31,25 +29,13 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
-        em.getTransaction().begin();
-         Task t = new Task();
+        // CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-         String content = "hello";
-         t.setContent(content);
+        // おまじないとしてのインスタンスを生成
+        request.setAttribute("message", new Task());
 
-         Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
-         t.setCreated_at(currentTime);
-         t.setUpdated_at(currentTime);
-
-         // データベースに保存
-         em.persist(t);
-         em.getTransaction().commit();
-
-         // 自動採番されたIDの値を表示
-         response.getWriter().append(Integer.valueOf(t.getId()).toString());
-
-         em.close();
-      }
-    }
-
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        rd.forward(request, response);
+     }
+}
